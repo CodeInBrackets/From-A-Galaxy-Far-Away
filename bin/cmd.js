216a84835bdf607aae6ffb8d9229e5c8506555ba
alvaro <Alvarojosepl@gmail.com>
2017-02-16 13:53:10 -0600

    added command line support

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,1 +1,17 @@
-console.log('this is the command line')
+var interpret = require('../lib/conversation')
+var readline = require('readline');
+var readline = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+readline.on('line', function(line){
+    if(line === `exit`)
+        process.exit()
+    var resultInterpret = interpret(line)
+    if(resultInterpret)
+        console.log(resultInterpret)
+})
+
+console.log('in a galaxy far far away...')
