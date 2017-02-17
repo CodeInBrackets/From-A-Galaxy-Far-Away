36e7ea55ac006ef0a1a06b6325485fb46694b84b
alvaro <Alvarojosepl@gmail.com>
2017-02-16 18:12:34 -0600

    did a bit of cleaning

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,14 +1,14 @@
 var interpret = require('../lib/conversation')
-var readline = require('readline');
-var readline = readline.createInterface({
+var readlineLib = require('readline');
+var readline = readlineLib.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
 readline.on('line', function(line){
-    if(line === `exit`)
-        process.exit()
+    if(line === `exit`) 
+        process.exit() 
     var resultInterpret = interpret(line)
     if(resultInterpret)
         console.log(resultInterpret)
